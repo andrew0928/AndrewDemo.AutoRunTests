15f0cbff1416adb4eb23401f8fd46d97dc5ebf72
Andrew Wu <chicken@chicken-house.net>
2025-04-16 02:08:23 +0800

    update prompt

--- OpenAPI/Program.cs
+++ OpenAPI/Program.cs
@@ -10,6 +10,7 @@ namespace OpenAPI
     {
         private static string OPENAI_APIKEY = null;
         private static string OPENAI_ORGID = null;
+        private static string KERNEL_MEMORY_APIKEY = null;
 
 
 
@@ -21,18 +22,19 @@ namespace OpenAPI
 
             OPENAI_APIKEY = config["OpenAI:ApiKey"];
             OPENAI_ORGID = config["OpenAI:OrgId"];
+            KERNEL_MEMORY_APIKEY = config["KernelMemory:ApiKey"];
 
             var builder = Kernel.CreateBuilder()
                 .AddOpenAIChatCompletion(
                     modelId: "gpt-4o-mini",
                     apiKey: OPENAI_APIKEY,
-                    httpClient: HttpLogger.GetHttpClient(true));
+                    httpClient: HttpLogger.GetHttpClient(false));
 
             var kernel = builder.Build();
 #pragma warning disable SKEXP0040 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
             HttpClient _hc = new HttpClient();
-            _hc.DefaultRequestHeaders.Add("Authorization", "1234567890.1234567890.1234567890");
+            _hc.DefaultRequestHeaders.Add("Authorization", KERNEL_MEMORY_APIKEY);
 
 
             await kernel.ImportPluginFromOpenApiAsync(
@@ -76,11 +78,29 @@ namespace OpenAPI
                     你是個 QA 測試人員
                     請依序執行下列步驟，並且用測試報告的格式輸出。
                     若最終符合預期結果，請傳回 "測試通過"。
+
+                    我期待測是要能涵蓋這些期待，請產生對應的情境，確保涵蓋這些預期的結果:
+                    1. 要能驗證參數的邊界範圍。例如 search( limit ), 必須是介於 1 ~ 5 的 integer, 而 index 在這邊必須是固定值 "blog"
+                    2. 相同的查詢必須傳回相同數量的結果與內容
+
                 </message>
                 <message role="user">
+                    測試的情境:
+
                     step 1, 搜尋 index: blog, "andrew"
                     step 2, 搜尋 index: blog, "sdk design"
                     step 3, 兩次搜尋至少都有一筆以上的資料回傳。
+                </message>
+                <message role="user">
+                    請重複數次主要情境，確保有涵蓋到邊界內，與超出邊界 (+-1) 的情境。
+                    報告的格式請列出:
+
+                    ## 測試編號 (預期說明)
+                    - 步驟 + 參數
+                      (呼叫資訊: function_name( parameters ): return result 摘要 )
+                      (預期結果 / 實際結果)
+                      
+                    ##(測試通過 / 測試失敗)
                 </message>
                 """,
                 new(settings)));
